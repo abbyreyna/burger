e76e3500b1df5f81d460a89def6b3e23d0235ac2
abbyreyna <reyna.abby08@gmail.com>
2019-05-09 22:05:03 -0700

    changed table name

--- models/burger.js
+++ models/burger.js
@@ -8,23 +8,23 @@ var orm = require("../config/orm.js");
 
 var burger = {
     all: function(cb) {
-      orm.all("burgers", function(res) {
+      orm.all("burger_db", function(res) {
         cb(res);
       });
     },
     // The variables cols and vals are arrays.
     create: function(cols, vals, cb) {
-      orm.create("burgers", cols, vals, function(res) {
+      orm.create("burgers_db", cols, vals, function(res) {
         cb(res);
       });
     },
     update: function(objColVals, condition, cb) {
-      orm.update("burgers", objColVals, condition, function(res) {
+      orm.update("burgers_db", objColVals, condition, function(res) {
         cb(res);
       });
     },
     delete: function(condition, cb) {
-      orm.delete("burgers", condition, function(res) {
+      orm.delete("burgers_db", condition, function(res) {
         cb(res);
       });
     }  
